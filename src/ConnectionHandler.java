6c5c5f4a6643067ac92daef2e27c89c7bd9918bf
Anthony Makepeace <makepeaceanthony@gmail.com>
2018-10-25 16:14:08 +0100

    Working image sending

--- src/ConnectionHandler.java
+++ src/ConnectionHandler.java
@@ -11,7 +11,9 @@ public class ConnectionHandler extends Thread {
     private Socket socket;
     private String directory;
     private int helper;
-    private File requestedFile;
+    private BufferedReader input;
+    private PrintWriter output;
+    private BufferedOutputStream buffOut;
 
     public ConnectionHandler(Socket socket, String directory){
         System.out.println(ThreadColor.ANSI_BLUE+"Connection Established, creating handler");
@@ -23,8 +25,9 @@ public class ConnectionHandler extends Thread {
     @Override
     public void run() {
         try{
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            buffOut = new BufferedOutputStream(socket.getOutputStream());
 
             while(true){
                 // Receive the input being sent
@@ -42,6 +45,8 @@ public class ConnectionHandler extends Thread {
                     System.out.println(ThreadColor.ANSI_GREEN+"String 1 = " + parsedRequest[1]);
                     String requestDirectory = directory + parsedRequest[1];
 
+                    File requestedFile;
+
                     if(new File(requestDirectory).isFile()) {
                         requestedFile = new File(requestDirectory);
                     } else {
@@ -50,22 +55,20 @@ public class ConnectionHandler extends Thread {
                         break;
                     }
 
+                    // Check what kind of file it is
+                    String contentType = checkContentType(requestedFile);
+
+                    if(requestType.equals("HEAD")){
+                        sendHEAD(requestedFile, contentType);
+                    }
+
                     if(requestType.equals("GET")){ // check if it is GET
-                        // return the header + file
-                        int fileLength = getFileLength(requestedFile);
-                        //byte[] content = getData(requestedFile);
-                        String content = getData(requestedFile);
-
-
-                        output.println("HTTP/1.1 200 OK");
-                        output.println("Server: Java HTTP Server by ACM35 : 1.0");
-                        output.println("Date: "+ new Date());
-                        output.println("Content-type: text/html");
-                        output.println("Content-length: " + fileLength);
-                        output.println();
-                        output.flush();
-                        output.println(content);
-                        output.flush();
+                        if(contentType.equals("image/jpeg")){
+                            sendGETImage(requestedFile, contentType);
+                        } else {
+                            //Send GET Data
+                            sendGET(requestedFile, contentType);
+                        }
                     }
                 }
             }
@@ -99,19 +102,6 @@ public class ConnectionHandler extends Thread {
         return (int)fileLength;
     }
 
-//    private byte[] getData(File requestedFile) throws IOException{
-//        FileInputStream fileIn = null;
-////        byte[] fileData = new byte[getFileLength(requestedFile)];
-////
-////        try{
-////            fileIn = new FileInputStream(requestedFile);
-////        } finally {
-////            if(fileIn != null)
-////                fileIn.close();
-////        }
-////
-////        return fileData;
-//    }
 
     private String getData(File requestedFile){
         try {
@@ -121,6 +111,66 @@ public class ConnectionHandler extends Thread {
             System.out.println("Get Data Exception: " + e.getMessage());
         }
         return "";
+    }
+
+    private String checkContentType(File file){
+        String fileName = file.getName();
+        if(fileName.endsWith(".htm") || fileName.endsWith("html")){
+            return "text/html";
+        } else if(fileName.endsWith(".jpg")){
+            return "image/jpeg";
+        } else {
+            return "text/plain";
+        }
+    }
+
+    private void sendHEAD(File file, String contentType){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Server: Java HTTP Server by ACM35 : 1.0");
+        output.println("Date: "+ new Date());
+        output.println("Content-type: "+ contentType);
+        output.println("Content-length: " + getFileLength(file));
+        output.println();
+        output.flush();
+    }
+
+    private void sendGET(File file, String contentType){
+        // return the header + file
+        output.println("HTTP/1.1 200 OK");
+        output.println("Server: Java HTTP Server by ACM35 : 1.0");
+        output.println("Date: "+ new Date());
+        output.println("Content-type: "+ contentType);
+        output.println("Content-length: " + getFileLength(file));
+        output.println();
+        output.flush();
+        output.println(getData(file));
+        output.flush();
+    }
+
+    private void sendGETImage(File file, String contentType){
+        output.println("HTTP/1.1 200 OK");
+        output.println("Server: Java HTTP Server by ACM35 : 1.0");
+        output.println("Date: "+ new Date());
+        output.println("Content-type: "+ contentType);
+        output.println("Content-length: " + getFileLength(file));
+        output.println();
+        output.flush();
+
+
+        try {
+            byte[] buffer = new byte[getFileLength(file)];
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                buffOut.write(buffer, 0, bytesRead);
+            }
+            output.println(getData(file));
+
+        } catch (IOException e){
+            System.out.println("SEND GET IMAGE EXCEPTION: " + e.getMessage());
+        }
+        output.flush();
     }
 
 }
