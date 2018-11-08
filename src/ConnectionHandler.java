534c7fc005e10db0b2dfc7fd7c1a51477fe2e713
antondubek <makepeaceanthony@gmail.com>
2018-11-08 21:18:58 +0000

    Get rid of .* imports

--- src/ConnectionHandler.java
+++ src/ConnectionHandler.java
@@ -1,4 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Date;
 import java.util.StringTokenizer;
